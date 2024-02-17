cb150aa8737bd342312f7f95a786633001bfca29
Assem Malgazhdarova <assem4491@gmail.com>
2024-02-17 12:39:59 -0500

    changed styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,32 +1,16 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import React from 'react';
+import logo from '../../assets/favicon.png';
+import './styles.css'; // Import your CSS file for styling
 
-import logo from '../../assets/favicon.png'
-import useStyles from './styles';
-
-const Navbar = ( ) => {
-    const classes = useStyles();
-
+const Navbar = () => {
     return (
-        <>
-        <AppBar position="fixed" className={classes.appBar} color="inherit">
-            <Toolbar>
-                <Typography variant="h6" className={classes.title} color="inherit">
-                    <img src={logo} alt="SafeWalkAI" height="25px" className={classes.image}/>
-                    SafeWalkAI
-                </Typography>
-                <div className={classes.grow} />
-                <div className={classes.button} >
-                    <IconButton arial-label="Safe Walk With Us" color="inherit">
-                        <Badge badgeContent={2} color="secondary">
-                            SafeWalkAI
-                        </Badge>
-                    </IconButton>
-                </div>
-            </Toolbar>
-        </AppBar>
-        </>
-    )
+        <div className="navbar">
+            <div className="logo-container">
+                <img src={logo} alt="SafeWalkAI" height="25px" className="logo" />
+                <span className="brand-name">SafeWalkAI</span>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
